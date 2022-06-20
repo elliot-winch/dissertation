37552f8401380f1a67deca69320a9bf9ed5cd0a3
unknown <erw331@nyu.edu>
2022-06-20 12:45:27 +0100

    Performance Measures

--- measure_performance.py
+++ measure_performance.py
@@ -1,4 +1,4 @@
-def mean_average_precision(self, matrix):
+def accuracy(matrix):
 
     if len(matrix) is not len(matrix[0]):
         print("Error: MAP cannot be calculated. Confusion matrix must be square")
@@ -14,3 +14,42 @@ def mean_average_precision(self, matrix):
                 total_correct += matrix[i][j]
 
     return total_correct / float(total)
+
+def error_rate(matrix):
+    return 1 - accuracy(matrix)
+
+def recall(matrix, row):
+    total = 0
+    total_correct = 0
+    for i in range(len(matrix[row])):
+        total += matrix[row][i]
+
+        if i is row:
+            total_correct += matrix[row][i]
+
+    return total_correct / float(total)
+
+def precision(matrix, column):
+    total = 0
+    total_correct = 0
+    for i in range(len(matrix)):
+        total += matrix[i][column]
+
+        if i is column:
+            total_correct += matrix[i][column]
+
+    return total_correct / float(total)
+
+def f1(matrix, _class):
+    prec = precision(matrix, _class)
+    rec = recall(matrix, _class)
+    return 2 * prec * rec / (prec + rec)
+
+if __name__ == '__main__':
+    matrix = [[1,2],[3,4]]
+
+    print(accuracy(matrix))
+    print(error_rate(matrix))
+    print(recall(matrix, 1))
+    print(precision(matrix, 1))
+    print(f1(matrix, 1))
